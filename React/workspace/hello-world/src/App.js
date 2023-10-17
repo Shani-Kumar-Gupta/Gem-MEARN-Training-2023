50925fa13d3515ed955f1a974769a22fc97d51bf
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2023-10-17 16:28:49 +0530

    Created first functional Component with default export

--- React/workspace/hello-world/src/App.js
+++ React/workspace/hello-world/src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React World!
-        </p>
-      </header>
+      <Greet />
     </div>
   );
 }
---
+++ React/workspace/hello-world/src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greet = () => {
+  return (
+    <div classNames='greet'>
+      <h1>Hello Shani!</h1>
+    </div>
+  );
+};
+
+export default Greet;
