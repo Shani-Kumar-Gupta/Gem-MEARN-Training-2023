af3f06fc4c838a1bdb572b7fd4c37b22b0104d4c
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2023-11-29 15:07:03 +0530

    learnt useState hook

--- React/workspace/react-hooks/src/App.js
+++ React/workspace/react-hooks/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import HookCounterFour from './components/HookCounterFour';
-import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/useState/HookCounterFour';
+import HookCounterThree from './components/useState/HookCounterThree';
 // import CounterClass from './components/CounterClass';
 // import HookCounter from './components/HookCounter';
 // import HookCounterTwo from './components/HookCounterTwo';
